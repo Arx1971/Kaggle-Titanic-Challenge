ff30ee5040938cf80d449fabbac5c626e4435052
Arx1971 <adnan.rahin13@gmail.com>
2019-12-27 06:44:16 -0500

    local Test data update

--- stochastic_ml_classifier/__init__.py
+++ stochastic_ml_classifier/__init__.py
@@ -1,9 +1,18 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-train_data = pd.DataFrame(pd.read_csv('train.csv'))
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+y = train_data["Survived"]
 
-total_survivor = train_data[train_data.Survived == 1]
-female_survivor = total_survivor[total_survivor.Sex == 'female']
-male_survivor = total_survivor[total_survivor.Sex == 'male']
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
 
-print(male_survivor)
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
+print("Your submission was successfully saved!")
