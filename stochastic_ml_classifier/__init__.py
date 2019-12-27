3a57267b49dfac6b1da93eb88683ebf43aa29154
Arx1971 <adnan.rahin13@gmail.com>
2019-12-27 03:53:12 -0500

    local Test data update

--- stochastic_ml_classifier/__init__.py
+++ stochastic_ml_classifier/__init__.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
-test_data = pd.read_csv('test.csv')
-print(test_data)
+train_data = pd.DataFrame(pd.read_csv('train.csv'))
+
+total_survivor = train_data[train_data.Survived == 1]
+female_survivor = total_survivor[total_survivor.Sex == 'female']
+male_survivor = total_survivor[total_survivor.Sex == 'male']
+
+print(male_survivor)
