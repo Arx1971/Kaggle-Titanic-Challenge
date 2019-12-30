f9ebfd5413e1755011d8f1e8932281a8bc5b9955
Arx1971 <adnan.rahin13@gmail.com>
2019-12-30 11:00:33 -0500

    Update

--- sikit_learn_classification/__init__.py
+++ sikit_learn_classification/__init__.py
@@ -9,3 +9,9 @@ features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'Bedroom
             'TotRmsAbvGrd']  # Extracting the features from original given data
 
 X = home_data[features]
+
+data_model = DecisionTreeRegressor(random_state=1)
+
+data_model.fit(X, Y)
+
+print(data_model.predict(X.head()))
