bffb7ac5aadfbe7c336771c611d0e91c3fc5a3f9
Arx1971 <adnan.rahin13@gmail.com>
2019-12-30 14:14:34 -0500

    update

--- sikit_learn_classification/__init__.py
+++ sikit_learn_classification/__init__.py
@@ -1,6 +1,16 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+
+def get_mae(max_leaf_nodes, train_x, val_x, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_x, train_y)
+    preds_val = model.predict(val_x)
+    mae = mean_absolute_error(val_y, preds_val)
+    return mae
+
 
 home_data = pd.read_csv('train.csv')
 
@@ -13,7 +23,17 @@ X = home_data[features]
 
 train_X, val_X, train_Y, val_Y = train_test_split(X, Y, random_state=0)
 
-data_model = DecisionTreeRegressor()
-data_model.fit(train_X, train_Y)
-val_predictions = data_model.predict(val_X)
+tree_sizes = [5, 25, 50, 100, 250, 500]
+min_val = get_mae(tree_sizes[0], train_X, val_X, train_Y, val_Y)
+index = 0
+print(min_val)
+for i in range(1, len(tree_sizes)):
+    temp = get_mae(tree_sizes[i], train_X, val_X, train_Y, val_Y)
+    print(temp)
+    if temp < min_val:
+        min_val = temp
+        index = i
 
+final_model = DecisionTreeRegressor(max_leaf_nodes=tree_sizes[index], random_state=0)
+final_model.fit(train_X, train_Y)
+val_predictions = final_model.predict(val_X)
--- stochastic_ml_classifier/__init__.py
+++ stochastic_ml_classifier/__init__.py
@@ -1,3 +1,20 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
 
+train_data = pd.read_csv('train.csv')
+test_data = pd.read_csv('test.csv')
+Y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = DecisionTreeRegressor(max_leaf_nodes=500, random_state=1)
+model.fit(X, Y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('my_submission.csv', index=False)
