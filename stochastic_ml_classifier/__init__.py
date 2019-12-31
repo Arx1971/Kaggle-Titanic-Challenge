c3007d9f176c07478062fe3b52a8990c9488b6e9
Arx1971 <adnan.rahin13@gmail.com>
2019-12-31 11:00:54 -0500

    Score 0.77511

--- stochastic_ml_classifier/__init__.py
+++ stochastic_ml_classifier/__init__.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 train_data = pd.read_csv('train.csv')
 test_data = pd.read_csv('test.csv')
@@ -9,10 +10,12 @@ features = ["Pclass", "Sex", "SibSp", "Parch"]
 
 X = pd.get_dummies(train_data[features])
 X_test = pd.get_dummies(test_data[features])
-model = RandomForestClassifier(n_estimators=500, random_state=1)
+
+model = DecisionTreeClassifier(max_leaf_nodes=15, random_state=1)
 model.fit(X, Y)
 predictions = model.predict(X_test)
-accuracy = model.score(X_test, predictions)
-print(accuracy)
+print(predictions)
+print(model.score(X_test, predictions))
+
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
 output.to_csv('my_submission.csv', index=False)
